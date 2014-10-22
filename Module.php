bcc3edb20a0ff98c4d1cdf946f814419a3b1fdb4
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-10-22 13:05:41 +0100

    base module upload

---
+++ Module.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace UthandoTestimonial;
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/config.php';
+    }
+    
+    public function getControllerConfig()
+    {
+        return include __DIR__ . '/config/controller.config.php';
+    }
+
+    public function getFormElementConfig()
+    {
+        return include __DIR__ . '/config/formElement.config.php';
+    }
+
+    public function getHydratorConfig()
+    {
+        return include __DIR__ . '/config/hydrator.config.php';
+    }
+
+    public function getInputFilterConfig()
+    {
+        return include __DIR__ . '/config/inputFilter.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return include __DIR__ . '/config/service.config.php';
+    }
+
+    public function getUthandoMapperConfig()
+    {
+        return include __DIR__ . '/config/mapper.config.php';
+    }
+
+    public function getUthandoModelConfig()
+    {
+        return include __DIR__ . '/config/model.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\ClassMapAutoloader' => [
+                __DIR__ . '/autoload_classmap.php'
+            ],
+        ];
+    }
+
+}
---
+++ config/config.php
@@ -0,0 +1,125 @@
+<?php
+return [
+    'uthando_user' => [
+        'acl' => [
+            'roles' => [
+                'guest' => [
+                    'privileges' => [
+                        'allow' => [
+                            'controllers' => [
+
+                            ],
+                        ],
+                    ],
+                ],
+                'admin' => [
+                    'privileges' => [
+                        'allow' => [
+                            'controllers' => [
+
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'resources' => [
+
+            ],
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'testimonial' => [
+                'type' => 'Literal',
+                'options' => [
+                    'route' => '/testimonial',
+                    'constraints' => [
+
+                    ],
+                    'defaults' => [
+                        '__NAMESPACE__' => 'UthandoTestimonial\Controller',
+                        'controller'    => 'Testimonial',
+                        'action'        => 'view',
+                        'force-ssl'     => 'http'
+                    ],
+                ],
+            ],
+            'admin' => [
+                'child_routes' => [
+                    'testimonial' => [
+                        'type' => 'Literal',
+                        'options' => [
+                            'route' => '/testimonial',
+                            'constraints'   => [
+                                'action'    => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ],
+                            'defaults' => [
+                                '__NAMESPACE__' => 'UthandoTestimonial\Controller',
+                                'controller'    => 'Testimonial',
+                                'action'        => 'index',
+                                'force-ssl'     => 'ssl'
+                            ],
+                        ],
+                        'may_terminate' => true,
+                        'child_routes' => [
+                            'edit' => [
+                                'type'    => 'Segment',
+                                'options' => [
+                                    'route'         => '/[:action[/id/[:id]]]',
+                                    'constraints'   => [
+                                        'action'    => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                        'id'		=> '\d+'
+                                    ],
+                                    'defaults'      => [
+                                        'action'        => 'edit',
+                                        'force-ssl'     => 'ssl'
+                                    ],
+                                ],
+                            ],
+                            'page' => [
+                                'type'    => 'Segment',
+                                'options' => [
+                                    'route'         => '/page/[:page]',
+                                    'constraints'   => [
+                                        'page' => '\d+'
+                                    ],
+                                    'defaults'      => [
+                                        'action'        => 'list',
+                                        'page'          => 1,
+                                        'force-ssl'     => 'ssl'
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'navigation' => [
+        'admin' => [
+            'testimonial' => [
+                'label' => 'Testimonials',
+                'pages' => [
+                    'list' => [
+                        'label'     => 'List All Testimonials',
+                        'action'    => 'list',
+                        'route'     => 'admin/testimonial',
+                        'resource'  => 'menu:user'
+                    ],
+                    'add' => [
+                        'label'     => 'Add New Testimonial',
+                        'action'    => 'add',
+                        'route'     => 'admin/testimonial/edit',
+                        'resource'  => 'menu:user'
+                    ],
+                ],
+                'route' => 'admin/testimonial',
+                'resource' => 'menu:user'
+            ],
+        ],
+    ],
+    'view_manager' => [
+        'template_map' => include __DIR__ . '/../template_map.php'
+    ],
+];
