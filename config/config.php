a98b93f00abaf979ada87698ba5477bb7cf10797
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-10-23 21:59:27 +0100

    moved menu to modules sub heading

--- config/config.php
+++ config/config.php
@@ -98,24 +98,28 @@ return [
     ],
     'navigation' => [
         'admin' => [
-            'testimonial' => [
-                'label' => 'Testimonials',
+            'modules' => [
                 'pages' => [
-                    'list' => [
-                        'label'     => 'List All Testimonials',
-                        'action'    => 'list',
-                        'route'     => 'admin/testimonial',
-                        'resource'  => 'menu:user'
-                    ],
-                    'add' => [
-                        'label'     => 'Add New Testimonial',
-                        'action'    => 'add',
-                        'route'     => 'admin/testimonial/edit',
-                        'resource'  => 'menu:user'
+                    'testimonial' => [
+                        'label' => 'Testimonials',
+                        'pages' => [
+                            'list' => [
+                                'label'     => 'List All Testimonials',
+                                'action'    => 'list',
+                                'route'     => 'admin/testimonial',
+                                'resource'  => 'menu:user'
+                            ],
+                            'add' => [
+                                'label'     => 'Add New Testimonial',
+                                'action'    => 'add',
+                                'route'     => 'admin/testimonial/edit',
+                                'resource'  => 'menu:user'
+                            ],
+                        ],
+                        'route' => 'admin/testimonial',
+                        'resource' => 'menu:user'
                     ],
                 ],
-                'route' => 'admin/testimonial',
-                'resource' => 'menu:user'
             ],
         ],
     ],
